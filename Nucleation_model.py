ff8abba4c4537d98ede58be514f9511beee6d91a
Melodie <mglasser@mines.edu>
2021-03-17 13:50:13 -0600

    Added concentration change

--- Nucleation_model.py
+++ Nucleation_model.py
@@ -13,49 +13,43 @@ from scipy.integrate import solve_ivp
 
 k_B = 1.380649E-23 #J K-1 // Boltzmann's constant
 ep_0 = 8.854E-12  #F m-1 // electric permittivity of a vacuum
-pi = 3.141529
 N_a = 6.022E23 #mol-1 // Avogadro's number
 R = 8.3145 #J mol-1 K-1 // Ideal constant
 
 """=========================== User system variables ==========================="""
 #Parameters that can be changed during the trials
-T = 25 + 273.15 #K
-C_LiO2 = 0.15 #mol m-3
-C_Li = 0.15 #mol m-3
-Elyte_v = 0.0005 #m3
-N_0 = 0
-A_0 = pi*0.009**2 #m2
-time = 100000
+T = 25 + 273.15 #K // Temperature
+C_LiO2_0 = 0.15 #mol m-3 // Electrolyte Concentration of LiO2 particles
+C_Li_0 = 0.15 #mol m-3 // Electrolyte Concentration Li+ particles
+Elyte_v = 500 #uL3 // Electrolyte Volume
+N_0 = 0 # // Nucleations
+A_0 = m.pi*0.009**2 #m2 // Cross sectional area
+time = 100000 #s // simulation time
 
 """=========================== User thermodyanic imputs ==========================="""
 #parameters that depend on the system being studied
 
-C_LiO2_sat = 0.1 #mol m-3 // Yin (2017)
-C_Li_sat = 0.1
-k_nu = 1E-6 #mol s-1 m-2 // Yin (2017)
-gamma_surf = 7.7E-3 #J m-2 // Danner (2019) - should be replaced for LiS system
-V = 1.98E-5 #m3 mol-1 // Yin (2017)
-theta = 30*pi/180 # radians // Danner (2019) - should be replaced for LiS system
-D_LiO2 = 1.2E-9 #m2 -s-1 // Yin (2017)
+C_LiO2_sat = 0.1 #mol m-3 // saturated concentration of LiO2 //Yin (2017)
+C_Li_sat = 0.1 #mol m-3 // saturated concentration of LiO2
+k_nu = 1E-6 #mol s-1 m-2 // kinetic rate constant for the nucleation reaction // Yin (2017)
+gamma_surf = 7.7E-3 #J m-2 //  surface energy of the newly formed crystal phase// Danner (2019) - should be replaced for LiS system
+V = 1.98E-5 #m3 mol-1 // molar volume// Yin (2017)
+theta = 30*m.pi/180 # radians // Contact angle // Danner (2019) - should be replaced for LiS system
+D_LiO2 = 1.2E-9 #m2 -s-1 // diffusion ceofficient // Yin (2017)
 
 
 """=========================== Thermodyanic system cals ==========================="""
-
-a_d = (C_LiO2*N_a)**(-1/3)
-phi = (2+m.cos(theta))*(1-m.cos(theta))**2/4
-r_crit = 2*gamma_surf*V/(R*T*m.log(C_LiO2/C_LiO2_sat*C_Li/C_Li_sat))
-N_crit = 4/3*pi*r_crit**3*N_a/V
-Del_G_Crit = phi*4/3*pi*gamma_surf*r_crit**2
-Z = m.sqrt(Del_G_Crit/(phi*3*pi*k_B*T*N_crit))
-
-N_sites_0 =  A_0/(pi*r_crit**2)
+Elyte_v_SI = Elyte_v*1.0E-9 #
+phi = (2+m.cos(theta))*(1-m.cos(theta))**2/4  # - // contact angle correction factor
 
 """=========================== Initialization step ==========================="""
 #%%
 #intializing the solution vector
 initial = {}
 initial['N'] = N_0
-initial['N_sites'] = N_sites_0
+initial['A'] = A_0
+initial['C_Li'] = C_Li_0
+initial['C_LiO2'] = C_LiO2_0
 sol_vec = list(initial.values())  # solution vector
 print(sol_vec)
 
@@ -64,21 +58,40 @@ print(sol_vec)
 
 
 def residual(t, solution):
-    N, N_sites = solution
+    N, A, C_Li, C_LiO2 = solution
+    a_d = (C_LiO2*N_a)**(-1/3) # length scale of diffusion
+    r_crit = 2*gamma_surf*V/(R*T*m.log(C_LiO2/C_LiO2_sat*C_Li/C_Li_sat)) # m // critical radius
+    N_crit = 4/3*m.pi*r_crit**3*N_a/V # number of molecules in the critical nucleus of size
+    Del_G_Crit = phi*4/3*m.pi*gamma_surf*r_crit**2 # J mol-1 // energy barrier of the nucleation
+    Z = m.sqrt(Del_G_Crit/(phi*3*m.pi*k_B*T*N_crit)) # - // Zeldovich factor
+    V_crit = 4/3*m.pi*r_crit**3 # m3 // Critical volume
+    N_sites = A/(m.pi*r_crit**2) # number of nucleation sites
     DN_Dt = D_LiO2*a_d*N_sites*Z*m.exp(-Del_G_Crit/(k_B*T))
-    DN_sites_Dt = - DN_Dt
-    return [DN_Dt, DN_sites_Dt]
+    DA_Dt = - DN_Dt*m.pi*r_crit**2
+    DCLi_Dt = -2*DN_Dt*V_crit/(V*Elyte_v_SI)
+    DCLiO2_Dt = -2*DN_Dt*V_crit/(V*Elyte_v_SI)
+    return [DN_Dt, DA_Dt, DCLi_Dt, DCLiO2_Dt]
 
 solution = solve_ivp(residual, [0, time], sol_vec)
 
 nucleations = solution.y[0]
-print(nucleations)
+area = solution.y[1]
+Li_concentration = solution.y[2]
+
+print(nucleations, Li_concentration)
 t = solution.t
 
 plt.figure(0)
 plt.plot(t,nucleations)
 plt.xlabel("Time (s)")
 plt.ylabel("Nucleation (#)")
+plt.show()
+plt.close()
+
+plt.figure(1)
+plt.plot(t,Li_concentration)
+plt.xlabel("Time (s)")
+plt.ylabel("Concentration (mol m-3)")
 plt.show()
 plt.close()
 """=========================== Citations ==========================="""

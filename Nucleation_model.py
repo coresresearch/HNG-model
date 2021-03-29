e21d98eedb1ddbc9f83cb8deb9bd4c97bea3e2e2
Melodie <mglasser@mines.edu>
2021-03-29 15:18:50 -0600

    Added radial growth doesn't finish running 🤔

--- Nucleation_model.py
+++ Nucleation_model.py
@@ -21,8 +21,9 @@ R = 8.3145 #J mol-1 K-1 // Ideal constant
 T = 25 + 273.15 #K // Temperature
 C_LiO2_0 = 0.15 #mol m-3 // Electrolyte Concentration of LiO2 particles
 C_Li_0 = 0.15 #mol m-3 // Electrolyte Concentration Li+ particles
-Elyte_v = 500 #uL3 // Electrolyte Volume
+Elyte_v = 1E-10 #uL3 // Electrolyte Volume
 N_0 = 0 # // Nucleations
+R_0 = 0
 A_0 = m.pi*0.009**2 #m2 // Cross sectional area
 time = 100000 #s // simulation time
 
@@ -32,7 +33,10 @@ time = 100000 #s // simulation time
 C_LiO2_sat = 0.1 #mol m-3 // saturated concentration of LiO2 //Yin (2017)
 C_Li_sat = 0.1 #mol m-3 // saturated concentration of LiO2
 k_nu = 1E-6 #mol s-1 m-2 // kinetic rate constant for the nucleation reaction // Yin (2017)
+k_surf = 1E-8 #mol s-1 m-2 // kinetic rate constant for the growth reaction // Yin (2017)
+k_surf_des = 1E-8 #mol s-1 m-2 // kinetic rate constant for the growth reaction // Yin (2017)
 gamma_surf = 7.7E-3 #J m-2 //  surface energy of the newly formed crystal phase// Danner (2019) - should be replaced for LiS system
+sig_surf = 0.75 # J m-2 // Specific surface energy of Li2O2 with the electrolyte // Yin (2017)
 V = 1.98E-5 #m3 mol-1 // molar volume// Yin (2017)
 theta = 30*m.pi/180 # radians // Contact angle // Danner (2019) - should be replaced for LiS system
 D_LiO2 = 1.2E-9 #m2 -s-1 // diffusion ceofficient // Yin (2017)
@@ -47,6 +51,7 @@ phi = (2+m.cos(theta))*(1-m.cos(theta))**2/4  # - // contact angle correction fa
 #intializing the solution vector
 initial = {}
 initial['N'] = N_0
+initial['R'] = R_0
 initial['A'] = A_0
 initial['C_Li'] = C_Li_0
 initial['C_LiO2'] = C_LiO2_0
@@ -58,7 +63,7 @@ print(sol_vec)
 
 
 def residual(t, solution):
-    N, A, C_Li, C_LiO2 = solution
+    N, R, A, C_Li, C_LiO2 = solution
     a_d = (C_LiO2*N_a)**(-1/3) # length scale of diffusion
     r_crit = 2*gamma_surf*V/(R*T*m.log(C_LiO2/C_LiO2_sat*C_Li/C_Li_sat)) # m // critical radius
     N_crit = 4/3*m.pi*r_crit**3*N_a/V # number of molecules in the critical nucleus of size
@@ -67,10 +72,12 @@ def residual(t, solution):
     V_crit = 4/3*m.pi*r_crit**3 # m3 // Critical volume
     N_sites = A/(m.pi*r_crit**2) # number of nucleation sites
     DN_Dt = D_LiO2*a_d*N_sites*Z*m.exp(-Del_G_Crit/(k_B*T))
-    DA_Dt = - DN_Dt*m.pi*r_crit**2
-    DCLi_Dt = -2*DN_Dt*V_crit/(V*Elyte_v_SI)
-    DCLiO2_Dt = -2*DN_Dt*V_crit/(V*Elyte_v_SI)
-    return [DN_Dt, DA_Dt, DCLi_Dt, DCLiO2_Dt]
+    DCLi_Dt = -DN_Dt*V_crit/(V*Elyte_v_SI)
+    DCLiO2_Dt = -DN_Dt*V_crit/(V*Elyte_v_SI)
+    R_avg = (N*R + DN_Dt*r_crit)/(N + DN_Dt) # m // average radius to be replaced later
+    Dravg_Dt = D_LiO2*V*(C_Li- C_Li_sat)*(C_LiO2-C_LiO2_sat)/(R_avg+D_LiO2/k_surf) - m.pi*R_avg**2*N*gamma_surf*k_surf_des
+    DA_Dt = - DN_Dt*m.pi*r_crit**2 - 2*m.pi*R_avg*Dravg_Dt
+    return [DN_Dt, Dravg_Dt, DA_Dt, DCLi_Dt, DCLiO2_Dt]
 
 solution = solve_ivp(residual, [0, time], sol_vec)
 
